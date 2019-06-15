c8754d126250be20344592a7cfbacc0fd6608a96
Karanvir Sawhney <kvss1992@users.noreply.github.com>
2019-06-15 03:46:46 -0400

    More refactoring and moving to custom hooks.

--- src/effect.js
+++ src/effect.js
@@ -1,21 +1,37 @@
 // useEffect: persistent state
 import React from "react";
 
-function useLocalStorageCounter({ step = 1, initialCount = 0, key = "count" }) {
-  const [count, setCount] = React.useState(() =>
-    Number(window.localStorage.getItem("count") || initialCount)
+function useLocalStorageState({
+  key,
+  initialValue,
+  serialize = v => v,
+  deserialize = v => v
+}) {
+  const [state, setState] = React.useState(() =>
+    deserialize(window.localStorage.getItem(key) || initialValue)
   );
+
+  const serializeState = serialize(state);
+  React.useEffect(() => {
+    window.localStorage.setItem(key, setState);
+  }, [key, serializeState]);
+
+  return [state, setState];
+}
+
+function useLocalStorageCounter({ step = 1, initialCount = 0, key = "count" }) {
+  const [count, setCount] = useLocalStorageState({
+    key,
+    initialCount,
+    deserialize: v => Number(v)
+  });
 
   const increment = () => setCount(c => c + step);
 
-  React.useEffect(() => {
-    window.localStorage.setItem("count", count);
-  });
-
   return [count, increment];
 }
-function Counter({ step = 1, initialCount = 0 }) {
-  const [count, increment] = useLocalStorageCounter(step, initialCount);
+function Counter({ step, initialCount }) {
+  const [count, increment] = useLocalStorageCounter({ step, initialCount });
 
   return <button onClick={increment}>{count}</button>;
 }
