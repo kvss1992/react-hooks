6254d304674510296d4992936fc1d9946d53bdb9
Karanvir Sawhney <kvss1992@users.noreply.github.com>
2019-06-15 03:15:57 -0400

    Starting with effect hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Usage from "./state";
+// import Usage from "./state";
+// import RefactorState from "./refactorState";
 
-import RefactorState from "./refactorState";
+import Effect from "./effect";
 
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<RefactorState />, rootElement);
+ReactDOM.render(<Effect />, rootElement);
